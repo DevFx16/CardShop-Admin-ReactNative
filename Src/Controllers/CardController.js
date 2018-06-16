c6ed54569c3dcfe65c3776a80eb88c182728617c
FernanPro18 <fernandoaraujo081@gmail.com>
2018-06-16 13:41:00 -0500

    Toast

---
+++ Src/Controllers/CardController.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const Url = 'https://cardshopadmin.herokuapp.com/Cards/';
+
+async function Get(Uri){
+  try {
+    let response = await fetch((Url+Uri));
+    let responseJson = await response.json();
+    console.log(responseJson);
+    return responseJson;
+  } catch (error) {
+   return null;
+  }
+}
+
+async function GetId(Uri, Id){
+  try {
+    let response = await fetch((Url+Uri+Id));
+    let responseJson = await response.json();
+    console.log(responseJson);
+    return responseJson;
+  } catch (error) {
+   return null;
+  }
+}
+
+async function Delete(Uri, Id){
+  try {
+    let response = await fetch((Url+Uri+Id));
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function Delete(Uri, Id){
+  try {
+    let response = await fetch((Url+Uri+Id), {method: 'DELETE',headers: {Accept: 'application/json','Content-Type': 'application/json'}});
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function Login(User, Password){
+  fetch((Url+'Login'),{method: 'POST',headers: {Accept: 'application/json','Content-Type': 'application/json',}, body: JSON.stringify({Username: User,Password: Password})})
+  .then((response) => {
+    console.log(response.status);
+    return response;
+  })
+  .catch((error)=>{
+    return null;
+  });
+}
+
+module.exports = {Get, GetId, Login, Delete}
--- Src/Views/LoginView.js
+++ Src/Views/LoginView.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import {ImageBackground, StatusBar} from 'react-native';
-import { Container, Content, Item, Icon, Input, Form, Footer, Button, Text, Spinner} from 'native-base';
+import {ImageBackground, StatusBar, NetInfo} from 'react-native';
+import { Container, Content, Item, Icon, Input, Form, Footer, Button, Text, Spinner, Toast, Root} from 'native-base';
+import Card from '../Controllers/CardController';
 
-const Image = require('../Images/Login.jpg')
+const Image = require('../Images/Login.jpg');
+var Toaste;
 
 export default class LoginView extends React.Component {
-  constructor() {
-    super();
-    this.state = {Font: false};
+  constructor(props) {
+    super(props);
+    this.state = {Font: false, Network: true, User: {Username: '', Password: ''}, Toast: ''};
   }
   componentDidMount(){
+    NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectionChange);
     StatusBar.setHidden(true);
+  }
+  handleConnectionChange = () => {
+    NetInfo.isConnected.fetch().then(isConnected => {
+      this.setState({ Network: isConnected });
+    });
   }
   async componentWillMount() {
     await Expo.Font.loadAsync({
@@ -19,30 +27,51 @@ export default class LoginView extends React.Component {
       'Ionicons': require("@expo/vector-icons/fonts/Ionicons.ttf"),
     });
     this.setState({Font: true});
+    
+  }
+  Login(){
+    console.log(this.state.User.Username);
+    if(this.state.User.Username.length <= 0 || this.state.User.Password.length <= 0){
+      this.setState({Toast: 'Se requieren los campos'});
+    }else{
+      let log = Card.Login('CardShop', 'CardShop');
+      var status = log.status;
+    }
+  }
+  Close = () => {
+    if(!this.state.ToastView){
+      this.setState({Toast: ''});
+    }
+  }
+  componentDidUpdate(){
+    Toaste = this.state.Toast.length > 0 ? Toast.show({onClose: this.Close, text: this.state.Toast, buttonText: "Okay",duration: 3000, buttonStyle: {backgroundColor: '#b33b3c'}}) : null 
   }
   render() {
     if(this.state.Font){
       return (
-        <ImageBackground source={Image} resizeMode='cover' style={{width: '100%', height: '100%'}}>
-          <Container>
-            <Content padder contentContainerStyle={{flex:1, justifyContent: 'flex-end'}}>
+        <Root>
+          <ImageBackground source={Image} resizeMode='cover' style={{width: '100%', height: '100%'}}>
+            <Container>
+              <Content padder contentContainerStyle={{flex:1, justifyContent: 'flex-end'}}>
                 <Form style={{marginRight: 10}}>
                   <Item>
                     <Icon active type='FontAwesome' name='user-circle' style={{color: 'white'}}/>
-                    <Input style={{color: 'white'}} placeholder="Username" />
+                    <Input style={{color: 'white'}} placeholder="Username" onChangeText={(Username) => this.setState({User: {Username: Username, Password: this.state.User.Password}})}/>
                   </Item>
                   <Item>
                     <Icon active type='MaterialIcons' name='vpn-key' style={{color: 'white', fontSize: 20,}}/>
-                    <Input style={{color: 'white'}} secureTextEntry={true} placeholder="Password" />
+                    <Input style={{color: 'white'}} secureTextEntry={true} placeholder="Password" onChangeText={(Password) => this.setState({User: {Username: this.state.User.Username, Password: Password}})}/>
                   </Item>
                 </Form>
-                <Button block rounded iconLeft style={{marginTop: 40, marginBottom: 40, backgroundColor:'#b33b3c' }}>
+                <Button block rounded iconLeft style={{marginTop: 40, marginBottom: 40, backgroundColor:'#b33b3c' }} onPress={this.Login.bind(this)}>
                   <Text>Login</Text>
                 </Button>
-            </Content>
-            <Footer style={{backgroundColor: 'rgba(0,0,0,0)'}}/>
-          </Container>
-        </ImageBackground>
+              </Content>
+              <Footer style={{backgroundColor: 'rgba(0,0,0,0)'}}/>
+           </Container>
+           {Toaste}
+          </ImageBackground>
+        </Root>
       );
     }else{
       return(
